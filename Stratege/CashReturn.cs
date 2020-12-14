dba10364770546741d9efe66aa587247f74e4518
Ben Huang <benxu.huang@gmail.com>
2020-12-14 21:59:01 +0800

    add CashReturn class

---
+++ Stratege/CashReturn.cs
@@ -0,0 +1,19 @@
+public class CashReturn : CashSuper
+{
+    private double moneyCondition = 0.0d;
+    private double moneyReturn = 0.0d;
+
+    public CashReturn(double moneyCondition, double moneyReturn)
+    {
+        this.moneyCondition = moneyCondition;
+        this.moneyReturn = moneyReturn;
+    }
+
+    public override double acceptCash(double money)
+    {
+        double result = money;
+        if (money >= moneyCondition)
+            result = money - Math.Floor(money / moneyCondition) * moneyReturn;
+        return result;
+    }
+}
