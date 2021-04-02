5967fbbae266a333eb50723c4f60cef203aa3d75
Ben Huang <benxu.huang@gmail.com>
2021-04-02 10:50:27 +0800

    add Singleton Pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+                Console.WriteLine("相同的實體");
+        }
+    }
+}
